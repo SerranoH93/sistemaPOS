fc57bea3f6cd9023e9f8960dd6fbbcc303fd4bdb
Alejandro Serrano <92839762+SerranoH93@users.noreply.github.com>
2024-04-02 14:23:08 -0600

    Inicializando

---
+++ front/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import { GlobalStyles } from './styles/GlobalStyles';
+
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <span>Hola mundo</span>        
+    </>
+  )
+}
+
+export default App
