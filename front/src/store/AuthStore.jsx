de9dad4e90273a23e3d2e17f763fd1bb81213271
Alejandro Serrano <92839762+SerranoH93@users.noreply.github.com>
2024-05-04 16:21:34 -0600

    Se agrega ejemplo para alertas

---
+++ front/src/store/AuthStore.jsx
@@ -0,0 +1,10 @@
+import Swal from 'sweetalert2';
+
+// Ejemplo de alertas faciles de usar
+
+Swal.fire({
+    icon: "error",
+    title: "Oops...",
+    text: "Something went wrong!",
+    footer: 'https://sweetalert2.github.io/#examples'
+});
