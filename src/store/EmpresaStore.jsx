7fa9b834475c9ac7747cfbb28ac11e89edbdd514
Alejandro Serrano <92839762+SerranoH93@users.noreply.github.com>
2024-06-06 14:09:30 -0600

    Stroe

---
+++ src/store/EmpresaStore.jsx
@@ -0,0 +1,9 @@
+import { create } from 'zustand';
+import { InsertarEmpresa } from '../supabase/curdEmpresa';
+
+export const useEmpresaStore = create((set) =>({
+    insertarempresa: async (p) => {
+        const response = await InsertarEmpresa(p)
+        console.log("respuesta empresa", response);
+    }
+}));
