e9ee5fcead050b7b9953138e357eda8bf9d5a30e
Alejandro Serrano <92839762+SerranoH93@users.noreply.github.com>
2024-07-13 10:15:12 -0600

    Fix

--- src/hooks/ProtectedRoute.jsx
+++ src/hooks/ProtectedRoute.jsx
@@ -1,6 +1,5 @@
 import { Navigate, Outlet } from 'react-router-dom';
 
-
 export const ProtectedRoute = ({ user, redirectTo, childen}) => {
     if(user === null) return <Navigate replace to={redirectTo} />;
     return childen ? childen : <Outlet />; //Con outlet le ayuda a mostrar una pantalla de carga
