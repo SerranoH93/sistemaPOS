6b1c04aa18981b7e1581f02e670859c266c72269
Alejandro Serrano <92839762+SerranoH93@users.noreply.github.com>
2024-07-13 17:03:53 -0600

    Se modifica crudCategorias para editar imagenes

--- src/supabase/crudCategorias.jsx
+++ src/supabase/crudCategorias.jsx
@@ -71,3 +71,64 @@ export async function MostrarCategorias(p){
         .order('id', { ascending: false });
     return data;
 }
+
+export async function BuscarCategorias (p) {
+    const {data} = supabase
+        .from(tabla)
+        .select
+        .eq("id_empresa", p.id_empresa)
+        .ilike("nombre", "%" + p.descripcion + "%");
+    return data;
+}
+
+export async function EliminarCategorias (p) {
+    const {error} = await supabase.from(tabla).delete.eq("id", p.id);
+    if (error) {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: error.message
+        });
+        return;
+    }
+
+    if(p.icono !="-") {
+        const ruta = "categorias/" + p.id;
+        await supabase.storage.from("imagenes").remove([ruta]);
+    }
+}
+
+export async function EditarCategorias(p, fileOld, fileNew) {
+    const {error} = await supabase.rpc("editarCategorias", p);
+
+    if (error) {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: error.message
+        });
+        return;
+    }
+
+    if(fileNew != "-" && fileNew.size != undefined) {
+        if(fileOld != "-") {
+            await EditarIconoStorage(p._id, fileNew)
+        } 
+        else {
+            const dataImagen = await subirImagen(p._id, fileNew)
+            const piconoeditar = {
+                icono: dataImagen.publicUrl,
+                id: p._id
+            };
+            await EditarIconoCategorias(piconoeditar);
+        }
+    }
+}
+
+export async function EditarIconoStorage(id, file) {
+    const ruta = "categorias/" + p.id;
+    await supabase.storage.from("imagenes").update(ruta, fiel, {
+        cacheControl: '0',
+        upsert: true
+    })
+}
